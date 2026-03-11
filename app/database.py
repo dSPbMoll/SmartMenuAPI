ff85e91f96f669d6fe3a032bd13199e0d166c76d
dSPbMoll <dsanchezpouso@cifpfbmoll.eu>
2026-03-11 22:55:38 +0100

    ENV variables commit

--- app/database.py
+++ app/database.py
@@ -8,25 +8,23 @@ load_dotenv()
 
 DATABASE_URL = os.getenv("DATABASE_URL")
 
-# Añadimos los argumentos de conexión SSL
+if not DATABASE_URL:
+    raise ValueError("ERROR: La variable DATABASE_URL no se encuentra configurada.")
+
 connect_args = {
     "ssl": {
-        "ca": "./isrgrootx1.pem" # Nombre exacto del archivo que descargaste
+        "ca": "./isrgrootx1.pem" 
     }
 }
 
 engine = create_engine(
     DATABASE_URL, 
-    connect_args=connect_args
+    connect_args=connect_args,
+    pool_pre_ping=True  # Recomendado para conexiones en la nube
 )
 
-if not DATABASE_URL:
-    raise ValueError("DATABASE_URL variable was not found in the .env file")
-
-engine = create_engine(DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
-
-Base = declarative_base() # Esta clase será la madre de tus tablas
+Base = declarative_base()
 
 def get_db():
     db = SessionLocal()
